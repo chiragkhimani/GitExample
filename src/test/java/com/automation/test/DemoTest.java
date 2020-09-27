f0d0ab477f6c9d32bfed0bc3ab707838c0fe2b6d
rose-jasmin-peacock <gpenjiyeva@gmail.com>
2020-09-27 11:29:14 -0700

    third addition

--- src/test/java/com/automation/test/DemoTest.java
+++ src/test/java/com/automation/test/DemoTest.java
@@ -3,6 +3,7 @@ package com.automation.test;
 public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("first line");
-		System.out.println("second line");
+		System.out.println("second line ");
+		System.out.println("third line from Guljan");
 	}
 }
