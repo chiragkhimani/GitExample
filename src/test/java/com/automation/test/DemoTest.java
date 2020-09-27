2e090f0964950ed237a7a667d4a0c41d8471853d
khimanichirag <khimanichirag@gmail.com>
2020-09-28 00:02:43 +0530

    From Chirag

--- src/test/java/com/automation/test/DemoTest.java
+++ src/test/java/com/automation/test/DemoTest.java
@@ -4,5 +4,6 @@ public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("first line");
 		System.out.println("second line");
+		System.out.println("thir line from chirag");
 	}
 }
